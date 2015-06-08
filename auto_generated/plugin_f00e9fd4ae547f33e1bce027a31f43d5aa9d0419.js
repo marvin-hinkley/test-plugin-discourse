b5f9a100cc31dd9bc4d13c8bfd5b32cbcf7d6912
madhouse <hinkley.marvin@gmail.com>
2015-06-08 14:24:40 -0700

    removed autogenerated nonsense

--- auto_generated/plugin_f00e9fd4ae547f33e1bce027a31f43d5aa9d0419.js
+++
@@ -1,2 +0,0 @@
-(function(){Discourse.LoginMethod.register(Discourse.LoginMethod.create({name: 'mvcp', titleOverride: 'with Microvellum Client Portal', messageOverride: 'Authenticating with Microvellum Client Portal (make sure pop up blockers are not enabled)', frameWidth: '1000', frameHeight: '800'}));
-})();
